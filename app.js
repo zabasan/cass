6a593629e7c4a04acc6422902d3ea3757243de86
zabasan <santiago.zabaleta@headwaydigital.com>
2017-07-15 16:49:50 -0300

    v1.01

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const userRouter = require('./routes/users');
@@ -8,7 +9,7 @@ const loginRouter = require('./routes/login');
 const authValidator = require('./helpers/authValidator');
 
 // Configuracion express
-
+app.use(express.static(path.join(__dirname, 'invitaciones/build')));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extend: false }));
@@ -22,6 +23,10 @@ app.use('/login', loginRouter());
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/casorio');
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/invitaciones/build/index.html'));
+});
 
 
 let servidor = 4000;
